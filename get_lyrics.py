5fae8cd0558d99f8895f773b8e10d47d2d7d7900
Hernan Razo <hernanrazo@gmail.com>
2018-09-15 00:26:35 -0500

    Added more data

--- get_lyrics.py
+++ get_lyrics.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 text_file = open('lyrics.txt', 'w')
 
 #scrape genius.com for whatever song you're looking for
-URL = 'https://genius.com/Trivium-vengeance-lyrics'
+URL = 'https://genius.com/Trivium-silence-in-the-snow-lyrics'
 page = requests.get(URL)
 html = BeautifulSoup(page.text, 'html.parser')
 lyrics = html.find('div', class_='lyrics').get_text().encode('ascii', 'ignore')
