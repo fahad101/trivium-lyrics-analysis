d16f925bbd492ac91bb3d2ad873a47d0cc344cc7
Hernan Razo <hernanrazo@gmail.com>
2018-10-16 00:12:53 -0500

    added to data

--- get_lyrics.py
+++ get_lyrics.py
@@ -4,12 +4,12 @@ from bs4 import BeautifulSoup
 
 #get lyrics from genius.com
 #open a text file that we can print the lyrics onto later
-data_path = '/Users/hernanrazo/pythonProjects/trivium_lyrics_analysis/data/vengeance_falls/'
-file_name = 'villainy_thrives.txt'
+data_path = '/Users/hernanrazo/pythonProjects/trivium_lyrics_analysis/data/silence_in_the_snow/'
+file_name = '.txt'
 text_file = open(data_path + file_name, 'wb')
 
 #scrape genius.com for whatever song you're looking for
-URL = 'https://genius.com/Trivium-villainy-thrives-lyrics'
+URL = ''
 page = requests.get(URL)
 html = BeautifulSoup(page.text, 'html.parser')
 lyrics = html.find('div', class_= 'lyrics').get_text().encode('ascii', 'ignore')
