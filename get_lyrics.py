fbe6c5768cbc8af592c9c66811808824464d9a05
Hernan Razo <hernanrazo@gmail.com>
2018-10-16 10:57:46 -0500

    added to data

--- get_lyrics.py
+++ get_lyrics.py
@@ -4,12 +4,12 @@ from bs4 import BeautifulSoup
 
 #get lyrics from genius.com
 #open a text file that we can print the lyrics onto later
-data_path = '/Users/hernanrazo/pythonProjects/trivium_lyrics_analysis/data/silence_in_the_snow/'
-file_name = '.txt'
+data_path = '/Users/hernanrazo/pythonProjects/trivium_lyrics_analysis/data/the_sin_and_the_sentence/'
+file_name = 'thrown_into_the_fire.txt'
 text_file = open(data_path + file_name, 'wb')
 
 #scrape genius.com for whatever song you're looking for
-URL = ''
+URL = 'https://genius.com/Trivium-thrown-into-the-fire-lyrics'
 page = requests.get(URL)
 html = BeautifulSoup(page.text, 'html.parser')
 lyrics = html.find('div', class_= 'lyrics').get_text().encode('ascii', 'ignore')
