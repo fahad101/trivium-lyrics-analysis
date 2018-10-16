4a63ce38c03323e9ffd9e5ec9c774ba021d390f4
Hernan Razo <hernanrazo@gmail.com>
2018-10-16 13:46:58 -0500

    added stop words functionality

--- script.py
+++ script.py
@@ -1,15 +1,17 @@
 
 import os
-import glob
 import csv
 import pandas as pd 
 import matplotlib.pyplot as plt
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
 
-#run this only the first time or if you don't have punkt already installed
-#nltk.download('punkt')
+#run this only the first time or if you don't have things already installed
+nltk.download('punkt')
+nltk.download('stopwords')
 
+#set general path for folder of albums
 data_path = ('/Users/hernanrazo/pythonProjects/trivium_lyrics_analysis/data/')
 
 #set strings for each album folder
@@ -23,26 +25,45 @@ album_folder7 = 'vengeance_falls/'
 album_folder8 = 'silence_in_the_snow/'
 album_folder9 = 'the_sin_and_the_sentence/'
 
+#set stop words to english
+stop_words = set(stopwords.words('english'))
 
-#TODO: clean this up. maybe one neat function??
-for root, dirs, files in os.walk(data_path + album_folder1, topdown = False):
-	for name in files:
-		if name != '.DS_Store': #prints .DS_Store sometimes for no reason, stop that
-			#print(os.path.join(name))
+#tokenize and filter lyrics
+def tokenize_and_filter(album_folder):
 
-			content = open(data_path + album_folder1 + name).read()
-			tokens = nltk.word_tokenize(content)
+	for root, dirs, files in os.walk(data_path + album_folder, topdown = False):
+		
+		for name in files:
 
-			print(tokens)
+			if name != '.DS_Store': #prints .DS_Store sometimes for no reason, stop that
+		
+				#open each file in the specific album folder
+				content = open(data_path + album_folder + name).read()
 
+				#tokenize the lyrics
+				tokenized_lyrics = nltk.word_tokenize(content)
 
+				#filter the tokenized lyrics
+				filtered_lyrics = [words for words 
+				in tokenized_lyrics if not words in stop_words]
 
+				filtered_lyrics = []
 
+				for words in tokenized_lyrics:
+					if words not in stop_words:
+						filtered_lyrics.append(words)
 
+				#print(tokenized_lyrics) #TODO: use this later for lyric generation??
+				return(filtered_lyrics)
+				print('lyrics tokenized and filtered')
 
-
-
-
-
-
-
+#tokenize lyrics
+tokenize_and_filter(album_folder1) #ember to inferno
+tokenize_and_filter(album_folder2) #trivium EP
+tokenize_and_filter(album_folder3) #ascendancy
+tokenize_and_filter(album_folder4) #the crusade
+tokenize_and_filter(album_folder5) #shogun
+tokenize_and_filter(album_folder6) #in waves
+tokenize_and_filter(album_folder7) #vengeance falls
+tokenize_and_filter(album_folder8) #silence in the snow
+tokenize_and_filter(album_folder9) #the sin and the sentence 
